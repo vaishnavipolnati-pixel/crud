81d083df4a9f296b09d7553de0d9e989c23fb067
POLNATI SRI SATYA VAISHNAVI <vaishnavi_polnati@srmap.edu.in>
2026-05-18 14:59:09 +0530

    Fixed frontend backend integration

--- fullstack-task/frontend/script.js
+++ fullstack-task/frontend/script.js
@@ -16,7 +16,7 @@
 
 // ==================== CONFIGURATION ====================
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = "http://127.0.0.1:8001"
 
 // API Endpoints
 const API_ENDPOINTS = {
